b43a1cd94263ac41e30c42973df15fa6ff3f8c6e
Serkan Şahin ZORLU <77185215+serkansahinz@users.noreply.github.com>
2023-09-14 16:36:48 +0300

    Add files via upload

---
+++ mail-service/src/main/java/com/bilgeadam/rabbitmq/RabbitMqConfig.java
@@ -0,0 +1,26 @@
+package com.bilgeadam.rabbitmq;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    @Value("${rabbitmq.register-queue}")
+    private String registerQueueName;
+    @Value("${rabbitmq.activation-queue}")
+    private String activationQueueName;
+
+    @Bean
+    Queue registerQueue(){
+        return new Queue(registerQueueName);
+    }
+
+
+    @Bean
+    public Queue activationQueue() {
+        return new Queue(activationQueueName);
+    }
+}
